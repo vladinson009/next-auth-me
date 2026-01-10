80e72fca331b2c16bcce961ea56e6222f9ee4d63
Vladimir Gulev <vladinson009@icloud.com>
2026-01-10 17:36:59 +0100

    Implemented: Loging user

---
+++ app/login/actions.ts
@@ -0,0 +1,34 @@
+'use server';
+
+import { signIn } from '@/auth';
+import { passwordSchema } from '@/validations/passwordSchema';
+import z from 'zod';
+
+type Props = {
+  email: string;
+  password: string;
+};
+export async function LoginWithCredentials({ email, password }: Props) {
+  const loginSchema = z.object({
+    email: z.email(),
+    password: passwordSchema,
+  });
+  const loginValidation = loginSchema.safeParse({
+    email,
+    password,
+  });
+
+  if (!loginValidation.success) {
+    return {
+      error: true,
+      message: loginValidation.error.issues[0].message ?? 'An error occured',
+    };
+  }
+  try {
+    await signIn('credentials', {
+      email,
+      password,
+      redirect: false,
+    });
+  } catch (error) {}
+}
---
+++ app/login/page.tsx
@@ -0,0 +1,96 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { passwordSchema } from '@/validations/passwordSchema';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import z from 'zod';
+import { LoginWithCredentials } from './actions';
+
+const formSchema = z.object({
+  email: z.email(),
+  password: passwordSchema,
+});
+
+export default function LoginPage() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  async function handleSubmit(data: z.infer<typeof formSchema>) {
+    await LoginWithCredentials({
+      email: data.email,
+      password: data.password,
+    });
+  }
+
+  return (
+    <main className="flex justify-center items-center min-h-screen">
+      <Card className="w-87.5">
+        <CardHeader>
+          <CardTitle>Login</CardTitle>
+          <CardDescription>Login to your account.</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(handleSubmit)}>
+              <fieldset
+                className="flex flex-col gap-2"
+                disabled={form.formState.isSubmitting}
+              >
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Password</FormLabel>
+                      <FormControl>
+                        <Input {...field} type="password" />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <Button type="submit">Login</Button>
+              </fieldset>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </main>
+  );
+}
