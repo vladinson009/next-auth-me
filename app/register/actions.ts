33459f7377a3df302a35d96f7371bc8529831754
Vladimir Gulev <vladinson009@icloud.com>
2026-01-10 14:17:13 +0100

    Added: Register form

---
+++ app/register/actions.ts
@@ -0,0 +1,31 @@
+'use server';
+
+import { passwordMatchSchema } from '@/validations/passwordMatchSchema';
+import z from 'zod';
+
+type Props = {
+  email: string;
+  password: string;
+  passwordConfirm: string;
+};
+
+export async function registerUser({ email, password, passwordConfirm }: Props) {
+  const newUserSchema = z
+    .object({
+      email: z.email(),
+    })
+    .and(passwordMatchSchema);
+
+  const newUserValidation = newUserSchema.safeParse({
+    email,
+    password,
+    passwordConfirm,
+  });
+
+  if (!newUserValidation.success) {
+    return {
+      error: true,
+      message: newUserValidation.error.issues[0].message ?? 'An error occured',
+    };
+  }
+}
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,3 +1,110 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { passwordMatchSchema } from '@/validations/passwordMatchSchema';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import z from 'zod';
+import { registerUser } from './actions';
+
+const formSchema = z
+  .object({
+    email: z.email(),
+  })
+  .and(passwordMatchSchema);
+
 export default function RegisterPage() {
-  return <div>RegisterPage</div>;
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+      passwordConfirm: '',
+    },
+  });
+
+  const handleSubmit = async (data: z.infer<typeof formSchema>) => {
+    const response = await registerUser({
+      email: data.email,
+      password: data.password,
+      passwordConfirm: data.passwordConfirm,
+    });
+    console.log(response);
+  };
+
+  return (
+    <main className="flex justify-center items-center min-h-screen">
+      <Card className="w-87.5">
+        <CardHeader>
+          <CardTitle>Register</CardTitle>
+          <CardDescription>Register for a new account.</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(handleSubmit)}
+              className="flex flex-col gap-2"
+            >
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input {...field} type="password" />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="passwordConfirm"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password confirm</FormLabel>
+                    <FormControl>
+                      <Input {...field} type="password" />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit">Register</Button>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </main>
+  );
 }
