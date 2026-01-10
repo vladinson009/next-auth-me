4a7f358b7a04bf1e397a13444391227358e418cf
Vladimir Gulev <vladinson009@icloud.com>
2026-01-10 18:58:47 +0100

    Added: Layouts

--- app/(logged-in)/logout-button/index.tsx
+++ app/(logged-in)/logout-button/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Button } from '../../components/ui/button';
+import { Button } from '@/components/ui/button';
 import logout from './actions';
 
 export default function LogoutButton() {
