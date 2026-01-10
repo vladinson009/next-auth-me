e1fa5f9587ba914b3131b8d66a07cdef202a6468
Vladimir Gulev <vladinson009@icloud.com>
2026-01-10 15:47:23 +0100

    Implemented: Register user

--- app/register/actions.ts
+++ app/register/actions.ts
@@ -1,8 +1,10 @@
 'use server';
 
-import db from '@/db/drizzle';
 import { passwordMatchSchema } from '@/validations/passwordMatchSchema';
 import z from 'zod';
+import { hash } from 'bcryptjs';
+import db from '@/db/drizzle';
+import { users } from '@/db/usersSchema';
 
 type Props = {
   email: string;
@@ -11,24 +13,52 @@ type Props = {
 };
 
 export async function registerUser({ email, password, passwordConfirm }: Props) {
-  // const result = await db.select()
+  try {
+    const newUserSchema = z
+      .object({
+        email: z.email(),
+      })
+      .and(passwordMatchSchema);
 
-  const newUserSchema = z
-    .object({
-      email: z.email(),
-    })
-    .and(passwordMatchSchema);
+    const newUserValidation = newUserSchema.safeParse({
+      email,
+      password,
+      passwordConfirm,
+    });
 
-  const newUserValidation = newUserSchema.safeParse({
-    email,
-    password,
-    passwordConfirm,
-  });
+    if (!newUserValidation.success) {
+      return {
+        error: true,
+        message: newUserValidation.error.issues[0].message ?? 'An error occured',
+      };
+    }
 
-  if (!newUserValidation.success) {
+    const hashedPassword = await hash(password, 10);
+
+    await db.insert(users).values({
+      email: email,
+      password: hashedPassword,
+    });
+  } catch (error: unknown) {
+    const dbError = error as {
+      cause?: {
+        code?: string;
+        constraint?: string;
+      };
+    };
+
+    if (
+      dbError.cause?.code == '23505' &&
+      dbError.cause?.constraint === 'users_email_unique'
+    ) {
+      return {
+        error: true,
+        message: 'Email is already in use',
+      };
+    }
     return {
       error: true,
-      message: newUserValidation.error.issues[0].message ?? 'An error occured',
+      message: 'An error occured1',
     };
   }
 }
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -22,6 +22,7 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import z from 'zod';
 import { registerUser } from './actions';
+import Link from 'next/link';
 
 const formSchema = z
   .object({
@@ -45,66 +46,85 @@ export default function RegisterPage() {
       password: data.password,
       passwordConfirm: data.passwordConfirm,
     });
-    console.log(response);
+    if (response?.error) {
+      form.setError('email', {
+        message: response.message,
+      });
+    }
   };
 
   return (
     <main className="flex justify-center items-center min-h-screen">
-      <Card className="w-87.5">
-        <CardHeader>
-          <CardTitle>Register</CardTitle>
-          <CardDescription>Register for a new account.</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <Form {...form}>
-            <form
-              onSubmit={form.handleSubmit(handleSubmit)}
-              className="flex flex-col gap-2"
-            >
-              <FormField
-                control={form.control}
-                name="email"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Email</FormLabel>
-                    <FormControl>
-                      <Input {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="password"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Password</FormLabel>
-                    <FormControl>
-                      <Input {...field} type="password" />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="passwordConfirm"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Password confirm</FormLabel>
-                    <FormControl>
-                      <Input {...field} type="password" />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <Button type="submit">Register</Button>
-            </form>
-          </Form>
-        </CardContent>
-      </Card>
+      {form.formState.isSubmitSuccessful ? (
+        <Card className="w-87.5">
+          <CardHeader>
+            <CardTitle>Your account has been created</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <Button asChild className="w-full">
+              <Link href="/login">Login to your account</Link>
+            </Button>
+          </CardContent>
+        </Card>
+      ) : (
+        <Card className="w-87.5">
+          <CardHeader>
+            <CardTitle>Register</CardTitle>
+            <CardDescription>Register for a new account.</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(handleSubmit)}>
+                <fieldset
+                  className="flex flex-col gap-2"
+                  disabled={form.formState.isSubmitting}
+                >
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Email</FormLabel>
+                        <FormControl>
+                          <Input {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Password</FormLabel>
+                        <FormControl>
+                          <Input {...field} type="password" />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="passwordConfirm"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Password confirm</FormLabel>
+                        <FormControl>
+                          <Input {...field} type="password" />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <Button type="submit">Register</Button>
+                </fieldset>
+              </form>
+            </Form>
+          </CardContent>
+        </Card>
+      )}
     </main>
   );
 }
